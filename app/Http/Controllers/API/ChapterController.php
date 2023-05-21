240bf333412ade4368acecdc8c09ab125f286b29
ngovansap123 <sapkuga@gmail.com>
2023-05-21 15:13:48 +0700

    CMD

---
+++ app/Http/Controllers/API/ChapterController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Chapter;
+use App\Repositories\UserId;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class ChapterController extends Controller
+{
+    /*
+    userId variable
+     */
+    public $userId;
+
+    public function __construct(UserId $userId)
+    {
+
+        $this->userId = $userId;
+
+    }
+
+    /*
+    validator update chapter
+     */
+    protected function validatorUpdateChapter(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'tenChuongHoc' => 'required|unique:chapters,tenChuongHoc,' . $request->id . ',id,user_id,' . Auth::guard('api')->user()->id,
+        ], [
+            'tenChuongHoc.required' => 'Tên chương học không bỏ trống',
+            'tenChuongHoc.unique' => 'Tên chương học đã tồn tại',
+        ]);
+    }
+    /*
+    create chapter
+     */
+    public function createChapter(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'tenChuongHoc' => 'required',
+        ], [
+            'tenChuongHoc.required' => 'Tên chương học không được bỏ trống',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $createChapter = Chapter::create([
+            'tenChuongHoc' => $request->tenChuongHoc,
+            'trangThai' => $request->trangThai,
+            'course_id' => $request->course_id,
+            'user_id' => $this->userId->returnUserId(),
+        ]);
+
+        return response()->json(['message' => 'Đăng chương học thành công', 'data' => $createChapter], 200);
+    }
+
+    /*
+    get chapter teacher
+     */
+    public function getChapter($id)
+    {
+        $getChapter = Chapter::where('course_id', $id)
+            ->with('lessons')
+            ->orderBy('id', 'desc')
+            ->get();
+        return response()->json(['data' => $getChapter]);
+    }
+
+    public function getChapterDetail($id)
+    {
+        $getChapterDetail = Chapter::where('id', $id)->first();
+        return response()->json(['data' => $getChapterDetail]);
+    }
+
+    /*
+    update chapter teacher
+     */
+    public function updateChapterTeacher(Request $request, $id)
+    {
+        $validator = $this->validatorUpdateChapter($request);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $chapter = Chapter::where('id', $id)
+            ->where('user_id', $this->userId->returnUserId())
+            ->first();
+
+        $chapter->update([
+            'tenChuongHoc' => $request->tenChuongHoc,
+            'trangThai' => $request->trangThai,
+            'course_id' => $request->course_id,
+            'user_id' => $this->userId->returnUserId(),
+        ]);
+
+        return response()->json(['message' => 'Cập nhật chương học thành công', 'data' => $chapter->fresh()], 200);
+    }
+
+    public function deleteChapterTeacher($id)
+    {
+        $deleteChapter = Chapter::findOrFail($id);
+
+        if ($deleteChapter && $deleteChapter->user_id == $this->userId->returnUserId()) {
+            $deleteChapter->delete();
+            return response()->json(['message' => 'Xóa chương học thành công'], 200);
+        }
+
+        return response()->json(['error' => 'Bạn không có quyền xóa'], 422);
+
+    }
+
+}
---
+++ app/Http/Controllers/Admin/AddUserController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use file;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Storage;
+
+class AddUserController extends Controller
+{
+    /*
+    Add User
+     */
+    public function addUser(UserRequest $request)
+    {
+
+        if ($request->hasFile('avatar')) {
+            $image = $request->file('avatar');
+            $file_name = $image->getClientOriginalName();
+            Storage::put('public/images/' . $file_name, file_get_contents($image));
+        }
+
+        $create = User::create([
+            'hoTen' => $request->hoTen,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'soDienThoai' => $request->soDienThoai,
+            'ngaySinh' => date('Y-m-d', strtotime($request->ngaySinh)),
+            'gioiTinh' => $request->gioiTinh,
+            'diaChi' => $request->diaChi,
+            'avatar' => $file_name,
+            'trangThai' => $request->trangThai,
+            'phanQuyen' => $request->phanQuyen,
+        ]);
+        return redirect()->back()->with('success', 'Thêm người dùng thành công');
+    }
+
+    public function userManagement()
+    {
+
+        $userId = Auth::user()->id;
+
+        $users = User::select('id', 'hoTen', 'avatar', 'phanQuyen', 'trangThai', 'email', 'soDienThoai', 'gioiTinh', 'ngaySinh', 'diaChi')
+            ->where('id', '!=', $userId)
+            ->where('phanQuyen', 1)
+            ->orderBy('last_seen', 'DESC')
+            ->paginate(5);
+
+        return view('Admin.userManagement.index', compact('users'));
+    }
+
+    public function userLecturers()
+    {
+        $userId = Auth::user()->id;
+        $users = User::select('id', 'hoTen', 'avatar', 'phanQuyen', 'trangThai', 'email', 'soDienThoai', 'gioiTinh', 'ngaySinh', 'diaChi')
+            ->where('id', '!=', $userId)
+            ->where('phanQuyen', 2)
+            ->orderBy('last_seen', 'DESC')
+            ->paginate(5);
+
+        return view('Admin.userManagement.index2', compact('users'));
+    }
+
+    public function userEdit($id)
+    {
+
+        $user = User::where('id', $id)->firstOrFail();
+
+        return view('Admin.userManagement.edit', compact('user'));
+
+    }
+
+    public function userUpdate(UpdateUserRequest $request, $id)
+    {
+
+        $user = User::find($id);
+        $data = [
+            'hoTen' => $request->hoTen,
+            'email' => $request->email,
+            'soDienThoai' => $request->soDienThoai,
+            'ngaySinh' => date('Y-m-d', strtotime($request->ngaySinh)),
+            'gioiTinh' => $request->gioiTinh,
+            'diaChi' => $request->diaChi,
+            'trangThai' => $request->trangThai,
+            'phanQuyen' => $request->phanQuyen,
+        ];
+
+        if ($request->hasFile('avatar')) {
+            $image = $request->file('avatar');
+            $file_name = $image->getClientOriginalName();
+            Storage::put('public/images/' . $file_name, file_get_contents($image));
+            $data['avatar'] = $file_name;
+        }
+
+        $user->update($data);
+
+        return redirect()->back()->with('success', 'Cập nhật thành công');
+
+    }
+
+    public function userDelete($id)
+    {
+        User::where('id', $id)->delete();
+
+        return redirect()->back()->with('success', 'Xóa người dùng thành công');
+
+    }
+}
