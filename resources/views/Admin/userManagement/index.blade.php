7c0c513ca01222ea4b42cb763a91e873966cb197
ngovansap123 <sapkuga@gmail.com>
2023-05-28 22:31:56 +0700

    CMD

--- resources/views/Admin/userManagement/index.blade.php
+++ resources/views/Admin/userManagement/index.blade.php
@@ -107,7 +107,7 @@
                                             </tbody>
                                         </table>
                                     </div>
-                                    @if ($users->count() >= 5)
+                                    @if ($users->count() >= 1)
                                         <div class="paginate-nhanVien"
                                             style=" display: flex; justify-content: center;margin-top: 20px ">
                                             {{ $users->links() }}
@@ -142,7 +142,7 @@
                                             </tbody>
                                         </table>
                                     </div>
-                                    @if ($users->count() >= 5)
+                                    @if ($users->count() >= 1)
                                         <div class="paginate-nhanVien"
                                             style=" display: flex; justify-content: center;margin-top: 20px ">
                                             {{ $users->links() }}
